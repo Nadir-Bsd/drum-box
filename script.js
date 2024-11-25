42fc93ac715dece8f4cf0f0028d0aa0933ba7686
Nadir-Bsd <bensadi.nadir42400@gmail.com>
2024-11-25 11:50:22 +0100

    UPDATE: animation and song work

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 // get
+const buttonPads = document.querySelectorAll(".key");
+const audios = document.querySelectorAll(".audio");
 
 // event
+document.addEventListener("keyup", playSong);
+document.addEventListener("keydown", animationAdd);
 
-// function
+
+// // function
+function playSong(event) {
+    const letterPress = event.keyCode;
+    audios.forEach((element) => {
+        if(element.dataset.key == letterPress){
+            element.play();
+        };
+    });
+};
+
+function animationAdd(event) {
+    const letterPress = event.keyCode;
+
+    buttonPads.forEach((element) => {
+        let buttonPress = element;
+
+        if(buttonPress.dataset.key == letterPress){
+            buttonPress.classList.add("playing");
+            
+            document.addEventListener("keyup", () => {
+                buttonPress.classList.remove("playing");            
+            });
+        };
+    });
+}
