a2815e2865f089a64e229e75e4ec6edc8221fe87
Nadir-Bsd <bensadi.nadir42400@gmail.com>
2024-11-27 21:51:58 +0100

    UPDATE: playsong(add) animes(work) but animation's play button (not work)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // get
 const buttonPads = document.querySelectorAll(".key");
 const audios = document.querySelectorAll(".audio");
-var register = [];
+var register;
 var recording = false;
 var start;
 
@@ -13,17 +13,12 @@ document.addEventListener("keydown", animationAdd);
 // // functions
 function pressKey(event) {
     const key = String(event.keyCode);
-
-    
     
     buttonPads.forEach((element) => {
-        
-        if(element.dataset.key === key && key !== "82"){
+        if(element.dataset.key === key && key !== "82" && key !== "80"){
             element.classList.toggle("playing");
         };
     });
-
-
 
     if(key === "82"){
         if(recording !== true){
@@ -34,19 +29,21 @@ function pressKey(event) {
     };
     
     if(recording === true){
-
         if(key !== "82"){
-        
             if (!start){
                 start = Date.now();
             };
-
-            console.log(register);
 
             if (Date.now === start){
                 register.push({"key": key, "time": 0});
             }else register.push({"key": key, "time": (Date.now() - start)});
         };
+    };
+
+    if(key === "80"){
+        if(register){
+            playSong(register);
+        }else{console.log(("aucun son enregistré"));}
     };
 
     // pour chaque audios, if l'audio = key qui est press alors play sound 
@@ -70,6 +67,42 @@ function animationAdd(event) {
     });
 };
 
-function playSong() {
+function playSong(notes) {
+
+    // foreach notes
+    notes.forEach((note) => {
+        // c'est la note qui est play
+        let noteWhoIsPlay = note.key;
+        // c'est le time de la note
+        let timeNoteIsPlay = note.time;
+        // c'est le time de la note plus time de l'anime 
+        let timeAnimationIsPlay = 180 + timeNoteIsPlay;
+
+        // cree une promesse
+        return new Promise(
+            // fait les timeOut
+            (resolve, reject) => {
+                // note timeOut
+                setTimeout(() => {
+
+                    const eventDown = new KeyboardEvent("keydown", {'keyCode': noteWhoIsPlay});
+                    document.dispatchEvent(eventDown);
+                    // joue les notes
+                    document.querySelector(`.audio[data-key='${noteWhoIsPlay}']`).play();
+                    
+                }, timeNoteIsPlay);  
+
+                setTimeout(() => {
+                    // cree un event
+                    const eventUp = new KeyboardEvent("keyup", {'keyCode':noteWhoIsPlay});
+                    // joue l'anime
+                    document.dispatchEvent(eventUp);
+                }, timeAnimationIsPlay);
+
+            }
+        ); 
+    });
 
 };
+
+playSong().then((res) => {console.log(res)}).catch((resErreur) => {console.log(resErreur)});
